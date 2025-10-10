a6030af7bb5c7681a01a4108e0cd975ddfa36d46
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-10-10 12:00:18 -0300

    create button component

--- src/components/navigationButtons.tsx
+++ src/components/navigationButtons.tsx
@@ -1,17 +1,17 @@
 "use client"
 
 import { useRouter } from "next/navigation";
+import { Button } from "./button";
 
 export function NavigationButton() {
   const router = useRouter();
 
   return (
     <nav className="flex gap-4">
-      <button onClick={() => router.push("/")}>INÍCIO</button>
-      <button>SOBRE</button>
-      <button>EXPERIÊNCIAS</button>
-      <button onClick={() => router.push("/projects")}>PROJETOS</button>
-      <button>CONTATO</button>
+      <Button onClick={() => router.push("/")}>SOBRE</Button>
+      {/* <button>EXPERIÊNCIAS</button> */}
+      <Button onClick={() => router.push("/projects")}>PROJETOS</Button>
+      {/* <button>CONTATO</button> */}
     </nav>
   );
 }
