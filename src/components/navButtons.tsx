43a2119c1ae1e1863de886a3be4f7038685b64e3
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-10-14 16:52:08 -0300

    adjust nav button

--- src/components/navButtons.tsx
+++ src/components/navButtons.tsx
@@ -3,7 +3,7 @@ import { ButtonLink } from "./buttonLink";
 export function NavButtons() {
   return (
     <nav className="flex gap-2 lg:gap-4">
-      <ButtonLink href="/">SOBRE</ButtonLink>
+      <ButtonLink href="/">INÍCIO</ButtonLink>
       {/* <button>EXPERIÊNCIAS</button> */}
       <ButtonLink href="/projects">PROJETOS</ButtonLink>
       <ButtonLink href="/contact">CONTATO</ButtonLink>
