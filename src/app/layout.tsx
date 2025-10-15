0a2054c845d5c3854721d107c4786a47103833bc
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-10-15 14:17:07 -0300

    add pdf to download, create dropdown button nac and adjust header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
-import "./globals.css";
-
 import { Metadata } from "next";
-import Image from "next/image";
-import logo from "@/assets/logo.png";
-import { NavButtons } from "@/components/navButtons";
 import { Toaster } from "react-hot-toast";
+import { HeaderMain } from "@/components/headerMain";
+
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Jefferson Oliveira",
@@ -18,11 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body cz-shortcut-listen="true" className="h-full flex flex-col">
-        <header className="flex z-20 w-full items-center justify-between px-4 xl:px-10 py-1 bg-[#F2F5FA] shadow-lg shadow-blue-700/10">
-          <Image src={logo} alt="logo" width={50} height={50} />
-
-          <NavButtons />
-        </header>
+        <HeaderMain />
 
         <main className="flex flex-1 bg-[url(/bg-projects.jpg)] bg-cover bg-no-repeat">
           {children}
